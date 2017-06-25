c3660b2395ab91db7d43f5d2d80f157054f5ca92
kodonad <dennisdick2014@gmail.com>
2017-06-25 12:49:31 +0200

    User Ressource komplett eingefügt

--- Revooks/dienstgeber/routes/user/index.js
+++ Revooks/dienstgeber/routes/user/index.js
@@ -1,10 +1,210 @@
+/* ** *****************************
+   *  REQUIRE
+   ** ***************************** 
+*/
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var bodyParser = require('body-parser');
 
+
+
+/* ** *****************************
+   *  METHODS
+   ** ***************************** 
+*/
+
+/* ** **************************************************************************
+   *  readUserFromFile
+   *  ---------------
+   *  liest alle Benutzer aus der JSON Datei aus, speichert sie in ein Array
+   *  und gibt sie anschließend zurück.
+   ** **************************************************************************
+*/
+function readUserFromFile(){
+    var content = fs.readFileSync('routes/user/json/user.json').toString();
+    var userList = "[]";
+    if(content.length > 0){
+      var tempContent = content.substr(0,content.length-1); // Um das letzte Komma aus dem Inhalt zu entfernen.
+      var contentObject = "["+tempContent+"]"; // Um aus den Objekten ein Array zumachen welches die Objekte beinhaltet.
+      var userList = JSON.parse(contentObject); // parsed den Inhalt der Datei in ein JSON Objekt.
+    }
+    return userList;
+}
+
+/* ** **************************************************************************
+   *  checkIfExistingUser
+   *  ---------------
+   *  Überprüft ob ein Benutzer schon Bereits in der JSON Datei vorhanden ist. 
+   ** **************************************************************************
+*/
+function checkIfExistingUser(user,userList){
+    var statusFree = true; // Sagt aus ob ein Benutzer vorhanden ist oder nicht, true = ist nicht vorhanden, false = ist vorhanden.
+    
+    for (var i = 0; i < userList.length;i++){
+        
+        if(userList[i].id === user.id){
+            statusFree = false;
+            break;
+        }    
+    }
+    
+    return statusFree;
+}
+
+/* ** **************************************************************************
+   *  updateFile
+   *  ---------------
+   *  aktualisiert die user.json Datei.
+   ** **************************************************************************
+*/
+function updateFile(userList){
+    
+    /* truncate dient dazu alle Benutzer aus der user.json zu löschen. Dadurch, dass bereits zuvor der Inhalt in die Variable userList 
+       geschrieben wurde, wird problemlos der Inhalt der Variable
+       userList, welche alle Benutzer enthält, in die user.json geschrieben.
+    */
+    fs.truncate("routes/user/json/user.json",0, function(err){
+               
+      for(var i = 0 ; i < userList.length;i++){ // schreibt jeden einzelnen Benutzer aus der Liste der Benutzer in die user.json
+                 
+            var writeLine = JSON.stringify(userList[i])+",";
+               
+            fs.appendFile("routes/user/json/user.json",writeLine, function(err){
+                     
+            });
+        }
+    });
+}
+
+
+/* ** *****************************
+   *  ROUTING
+   ** ***************************** 
+*/
+
+/* ---------------------------------
+   >>  Ressource (user/)
+   ---------------------------------
+*/
+
+/* ** GET ** */
 router.get('/',function(req,res){
-    res.send('Hier stehen alle User');
+    var userList = readUserFromFile();
+    res.status(200).send(userList);
 });
 
+/* ** POST ** */
+router.post('/',bodyParser.json(),function(req,res){
+    var userObject = req.body; // der hinzuzufügende Benutzer
+    var userList = readUserFromFile();
+    
+    
+    var check = checkIfExistingUser(userObject,userList);
+    
+    if(check){
+        var writeLine = JSON.stringify(userObject)+","; // das "," dient dazu innerhalb der JSON Datei die einzelnen Benutzer aufzulisten.
+        fs.appendFile("routes/user/json/user.json",writeLine,function(err){
+        if(err){console.log(err);}             
+        });
+        res.status(201).send("Benutzer wurde erfolgreich erstellt.");
+    }
+    else{
+        res.status(400).send("Es gibt bereits schon einen Benutzer mit der ID: "+userObject.id);
+    }
+    
+    
+});
+
+/* ---------------------------------
+   >>  Ressource (user/:id)
+   ---------------------------------
+*/
+
+/* ** GET ** */
+router.get('/:id',function(req,res){
+    var reqID = req.params.id; // der Wert der ID.
+    var userList = readUserFromFile(); // liest die Benutzer aus der JSON Datei aus.
+    
+    var exists = false;
+    
+    for(var i = 0 ; i < userList.length;i++ )
+    {
+      if(userList[i].id === reqID){ // falls einer der Benutzer die ID hat die mit der request id übereinstimmen.
+          exists = true;
+          res.status(200).send(userList[i]);
+          break;
+      }
+    }
+    
+    if(exists === false){
+        res.status(400).send("Es existiert kein Benutzer mit dieser ID");
+    }
+});
+/* ** PUT ** */
+router.put('/:id',bodyParser.json(),function(req,res){
+    var reqID = req.params.id; // die ID des abzuänderten Benutzers.
+    var newUser = req.body; // Die Informationen des neu zu speichernden Benutzers
+    var userList = readUserFromFile(); // liest die Benutzer aus der JSON Datei aus.
+    
+    var exists = false;
+    
+    for (var i = 0; i < userList.length; i++){
+        
+        if(userList[i].id === reqID){ // falls eines der Benutzer die ID hat die mit der request id übereinstimmen.
+            
+            var userToChange; // =  Aktueller Benutzer ohne Änderung der Benutzerinformationen.
+            var changedUser; // = neuer Benutzer mit Änderungen der Benutzerinformationen.
+            
+            exists = true;
+            userToChange = userList[i];
+            
+            changedUser = userToChange; // Schreibt den aktuellen Benutzer in die changedUser Variable, dessen Attribute überschrieben werden.
+            
+            for (var attrNew in newUser){ // für jedes Attribut in der abgeänderten Benutzerinformation.
+        
+                for (var attr in changedUser){ // für jedes Attribut in der jetzigen Benutzerinformation. 
+            
+                    if(attrNew === attr){
+                    changedUser[attr] = newUser[attrNew]; // überschreibt den Wert des Attributs mit dem des neuen.
+                    }
+                }
+            
+            }
+         userList[i] = changedUser; // überschreibt den Benutzer aus der Benutzerliste.
+         
+        updateFile(userList); // Aktualisiert die JSON Datei.
+        
+        res.status(201).send("Benutzerinformationen erfolreich aktualisiert.");
+        }
+        
+    }
+    if(exists === false){
+        res.status(400).send("Es existiert kein Benutzer mit dieser ID");
+    }
+});
+
+/* ** DELETE ** */
+router.delete('/:id',function(req,res){
+    var reqID = req.params.id; // der Wert der ID.
+    var userList = readUserFromFile(); // liest die Bücher aus der JSON Datei aus.
+    
+    var exists = false;
+    for(var i = 0;i < userList.length;i++){
+       if(userList[i].id === reqID){ // falls eines der Bücher die ID hat die mit der request id übereinstimmen.
+           exists = true;
+           
+           userList.splice(i,1); // entfernt den Benutzer aus der Liste. (i = die aktuelle Stelle im Array)
+           console.log("userList: "+userList);
+           updateFile(userList);
+         res.status(200).send("der Benutzer wurde erfolgreicht gelöscht.");
+       }
+    }
+    if(exists === false){
+        res.status(400).send("Es existiert kein Benutzer mit dieser ID");
+    }
+     
+});
 
 //Bereitstellen des Moduls um require in der app.js einbinden zu können.
 module.exports = router;
