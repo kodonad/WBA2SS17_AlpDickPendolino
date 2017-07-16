7ede7090a207bdc8f71a9b44c488346c805ee936
kodonad <dennisdick2014@gmail.com>
2017-07-16 20:54:46 +0200

    Dienstnutzer Bücher Ressource implementiert (Nur Get auf books/ und auf books/:id)

--- Revooks/dienstnutzer/routes/books/index.js
+++ Revooks/dienstnutzer/routes/books/index.js
@@ -41,3 +41,28 @@ router.get('/',function(req,res){
          }
     })
 });
+
+
+/* ** GET ** */
+router.get('/:id',function(req,res){
+    var id = req.params.id;
+    var reqUrl = serviceUrl+'/books/'+id;
+    request.get(reqUrl,function(error,response,body){
+        switch(response.statusCode){
+            case 200: res.status(response.statusCode).send(JSON.parse(body));
+                break;
+            case 404: res.status(response.statusCode).send(body);
+                break;
+            default:break;
+         }
+    })
+});
+
+
+
+
+
+
+
+//Bereitstellen des Moduls um require in der app.js einbinden zu können.
+module.exports = router;
